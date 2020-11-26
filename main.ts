4b817d3c331b3864c1df440440025b6bda90a71c
mercierma376 <56010745+mercierma376@users.noreply.github.com>
2020-11-26 10:52:45 -0500

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,36 @@
+input.onButtonPressed(Button.B, function () {
+    pins.digitalWritePin(DigitalPin.P7, 1)
+    basic.pause(100)
+    reading = pins.analogReadPin(AnalogPin.P3)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P7, 0)
+    pourcentage = Math.round((reading - 610) * 100 / (1023 - 610))
+    basic.showString("" + pourcentage + "%")
+})
+let pourcentage = 0
+let reading = 0
+radio.setGroup(1)
+led.setBrightness(50)
+servos.P2.setAngle(0)
+basic.pause(1000)
+servos.P2.stop()
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P7, 1)
+    basic.pause(100)
+    reading = pins.analogReadPin(AnalogPin.P3)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P7, 0)
+    pourcentage = Math.round((reading - 610) * 100 / (1023 - 610))
+    if (pourcentage <= 80) {
+        servos.P2.setAngle(180)
+    } else if (pourcentage >= 80 && pourcentage <= 95) {
+        servos.P2.stop()
+    } else if (pourcentage > 95) {
+        servos.P2.setAngle(0)
+    }
+    radio.sendValue("H sol", pourcentage)
+    basic.pause(500)
+    servos.P2.stop()
+    // 30 secondes avant la prochaine mesure.
+    basic.pause(5000)
+})
